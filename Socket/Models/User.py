480259bc49dd02f0e9b32665feb8a7aab896af06
inugami-dev64 <karl@karlott.net>
2023-04-10 17:13:25 +0300

    Finish CRUD operations for User dataclass

--- Socket/Models/User.py
+++ Socket/Models/User.py
@@ -60,6 +60,48 @@ class User(UserDTO):
 
 
     @staticmethod
+    def ValidateUsername(cur: mariadb.Cursor, Username: str):
+        """Check if the given username is not already present in the database
+        :param cur: mariadb cursor to use
+        :param Username: username to verify
+        """
+
+        try:
+            cur.execute(
+                "SELECT 1 FROM Users WHERE Username=?",
+                (Username,)
+            )
+        except mariadb.Error as e:
+            logging.error(e)
+
+        for (UserExists,) in cur:
+            return False
+
+        return True
+
+
+    @staticmethod
+    def ValidateEmail(cur: mariadb.Cursor, Email: str):
+        """Check if the given email is not already present in the database
+        :param cur: mariadb cursor to use
+        :param Email: email to verify
+        """
+
+        try:
+            cur.execute(
+                "SELECT 1 FROM Users WHERE Email=?",
+                (Email,)
+            )
+        except mariadb.Error as e:
+            logging.error(e)
+
+        for (EmailExists,) in cur:
+            return False
+
+        return True
+
+
+    @staticmethod
     def Create(Username: str, Email: str, Password: str):
         """Create a new user
         :param Username: username
@@ -71,17 +113,14 @@ class User(UserDTO):
         cur = conn.cursor()
 
         # Check if user with Username already exists
-        try:
-            cur.execute(
-                "SELECT 1 FROM Users WHERE Username=?",
-                (Username,)
-            )
-        except mariadb.Error as e:
-            logging.error(e)
-
-
-        for (UserExists,) in cur:
+        if not User.ValidateUsername(cur, Username):
+            conn.close()
             return { "error": f"User with username '{Username}' already exists" }
+
+        # Check if user with given email already exists
+        if not User.ValidateEmail(cur, Email):
+            conn.close()
+            return { "error": f"User with email '{Email}' already exists'" }
 
         # Username should be unique, add it to the database
         salt = bcrypt.gensalt()
@@ -122,3 +161,213 @@ class User(UserDTO):
 
         conn.close()
         return { "error": "Could not create a new user.\nTry again!" }
+
+
+    @staticmethod
+    def Read(Username: str = '', Email: str = '', Password: str = ''):
+        """Non DTO User Read method.
+        Requires authenctication!
+        :param Username: if this is specified authenticate using Username as user identifier
+        :param Email: if this is specified authenticate using Email as user identifier
+        :param Password: password to use for authentication process
+        """
+
+        conn = SQLConnector.Connect()
+        cur = conn.cursor()
+
+        # Authenticate using Username
+        if Username != '':
+            try:
+                cur.execute(
+                    "SELECT * FROM Users WHERE Username=?",
+                    (Username,)
+                )
+            except mariadb.Error as e:
+                logging.error(e)
+
+        elif Email != '':
+            try:
+                cur.execute(
+                    "SELECT * FROM Users WHERE Email=?",
+                    (Email,)
+                )
+            except mariadb.Error as e:
+                logging.error(e)
+
+        else:
+            conn.close()
+            return { "error": "Email or username field is required" }
+
+
+        for (Id,Username,CreationTimestamp,Email,PasswordHash) in cur:
+            # Authenticate using bcrypt
+            if not bcrypt.checkpw(Password.encode('utf-8'), PasswordHash.encode('utf-8')):
+                return { "error": "Invalid password" }
+
+            user = User(
+                Id,
+                Username,
+                int(time.mktime(CreationTimestamp.timetuple())),
+                Email,
+                Password
+            )
+
+            conn.close()
+            return user.__dict__
+
+        conn.close()
+        return { "error": "Invalid username or email" }
+
+    
+    @staticmethod
+    def Update(Username='', Email='', Password='', NewUsername='', NewEmail='', NewPassword=''):
+        """Update user data
+        :param Username: if specified, use given username for authentication
+        :param Email: if specified, use given email address for authentication
+        :param Password: password to use for authentication
+        :param NewUsername: if not empty update username
+        :param NewEmail: if not empty update email
+        :param NewPassword: if not empty update password
+        """
+
+        conn = SQLConnector.Connect()
+        cur = conn.cursor()
+
+        if Username != "":
+            try:
+                cur.execute(
+                    "SELECT * FROM Users WHERE Username=?",
+                    (Username,)
+                )
+            except mariadb.Error as e:
+                logging.error(e)
+
+        elif Email != "":
+            try:
+                cur.execute(
+                    "SELECT * FROM Users WHERE Email=?",
+                    (Email,)
+                )
+            except mariadb.Error as e:
+                logging.error(e)
+
+        else:
+            conn.close()
+            return { "error": "Email or username field is required" }
+
+
+        user = User(0, '', 0, '', '')
+        isEntry = False
+        for (Id,Username,CreationTimestamp,Email,PasswordHash) in cur:
+            # Authenticate
+            if not bcrypt.checkpw(Password.encode('utf-8'), PasswordHash.encode('utf-8')):
+                return { "error": "Invalid password" }
+
+            if NewUsername == '':
+                NewUsername = Username
+            elif not User.ValidateUsername(cur, NewUsername):
+                conn.close()
+                return { "error": f"Cannot change username to '{NewUsername}', username already exists" }
+
+            if NewEmail == '':
+                NewEmail = Email
+            elif not User.ValidateEmail(cur, NewEmail):
+                conn.close()
+                return { "error": f"Cannot change email to '{NewEmail}', email already exists" }
+
+            if NewPassword == '':
+                NewPassword = PasswordHash
+            else:
+                salt = bcrypt.gensalt()
+                NewPassword = bcrypt.hashpw(NewPassword.encode('utf-8'), salt).decode('utf-8')
+    
+            user.Id = Id
+            user.Username = NewUsername
+            user.CreationTimestamp = int(time.mktime(CreationTimestamp.timetuple()))
+            user.Email = NewEmail
+            user.Password = NewPassword
+            isEntry = True
+            break
+
+        if not isEntry:
+            conn.close()
+            return { "error": "Invalid username or email" }
+
+        try:
+            cur.execute(
+                "UPDATE Users SET Username=?, Email=?, Password=? WHERE Id=?",
+                (user.Username, user.Email, user.Password, user.Id)
+            )
+            conn.commit()
+        except mariadb.Error as e:
+            logging.error(e)
+        
+        conn.close()
+        return user.__dict__
+
+
+    @staticmethod
+    def Delete(Username='', Email='', Password=''):
+        """Delete given user
+        :param Username: if specified, use given username for authentication
+        :param Email: if specified, use given email for authentication
+        :param Password: password to use for authentication
+        """
+
+        conn = SQLConnector.Connect()
+        cur = conn.cursor()
+
+        if Username != '':
+            try:
+                cur.execute(
+                    "SELECT * FROM Users WHERE Username=?",
+                    (Username,)
+                )
+            except mariadb.Error as e:
+                logging.error(e)
+
+        elif Email != '':
+            try:
+                cur.execute(
+                    "SELECT * FROM Users WHERE Email=?",
+                    (Email,)
+                )
+            except mariadb.Error as e:
+                logging.error(e)
+        else:
+            conn.close()
+            return { "error": "Email or username field is required" }
+
+
+        isEntry = False
+        user = User(0, '', 0, '', '')
+        for (Id,Username,CreationTimestamp,Email,PasswordHash) in cur:
+            user.Id = Id
+            user.Username = Username
+            user.CreationTimestamp = int(time.mktime(CreationTimestamp.timetuple()))
+            user.Email = Email
+            user.Password = PasswordHash
+            isEntry = True
+
+        if not isEntry:
+            conn.close()
+            return { "error": "Invalid username or email" }
+        
+        # Authenticate
+        if not bcrypt.checkpw(Password.encode('utf-8'), user.Password.encode('utf-8')):
+            conn.close()
+            return { "error": "Invalid password" }
+
+        # attempt to delete the user
+        try:
+            cur.execute(
+                "DELETE FROM Users WHERE Id=?",
+                (user.Id,)
+            )
+            conn.commit()
+        except mariadb.Error as e:
+            logging.error(e)
+
+
+        conn.close()
+        return user.__dict__
